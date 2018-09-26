b6d1cdfbdf33c8880e47e6d873c5abedbaf7f9ff
SowaTheKing <Zqbeirrt!23>
2018-09-26 19:24:41 +0200

    Initial local bus API integration

--- predict_reply.py
+++ predict_reply.py
@@ -32,7 +32,7 @@ def classify(msg):
         śmierdzących polaczków."""
 
     if (msg.find("rozklad") == 0):
-        return ztm()
+        return "Srosklad"
 
     if (msg.find("weather") == 0):
         try:
